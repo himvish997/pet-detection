c6c5478521c5e4bec6c872330453fc1540096d1d
Himanshu Vishwakarma <himvish997@gmail.com>
2018-10-16 08:06:17 +0530

    license in main and .gitignore is added

---
+++ main.py
@@ -0,0 +1,23 @@
+'''# Pet-Detection - main.py
+   #
+   # Copyright (C) 2018 Himanshu Vishwakarma <himvish997@gmail.com>
+   #
+   # Authors:
+   #   Himanshu Vishwakarma <himvish997@gmail.com>
+   #
+   #   This program is free software; you can redistribute it and/or modify
+   #   it under the terms of the GNU General Public License as published by
+   #   the Free Software Foundation; either version 3 of the License, or
+   #   (at your option) any later version.
+   #
+   #   This program is distributed in the hope that it will be useful,
+   #   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   #   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   #   GNU General Public License for more details.
+   #
+   #   You should have received a copy of the GNU General Public License
+   #   along with this program; if not, see <https://www.gnu.org/licenses/>.
+   #'''
+
+print("Welcome to the pet-detection program, Here we learn many technique for"
+      " the detection and localization of the obejct in the image")
