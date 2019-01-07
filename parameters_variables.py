9db8c28b8ab03a2cafe03303526057700df21f44
Himanshu Vishwakarma <himvish997@gmail.com>
2019-01-07 15:28:47 +0530

    added parameters_variables.py, contains parameters and variable used in project

---
+++ parameters_variables.py
@@ -0,0 +1,13 @@
+# paths of various location
+
+images_path = "./dataset/images/"
+annotations_path = "./dataset/annotations/xmls/"
+tfrecord_path = 'pets_data.tfrecord'
+
+# classes
+classes = ['cat', 'dog']
+
+
+# Model _Parameters
+IMG_SIZE = 416
+N_classes = 2
